72df938b29498e257ee7d084bc8679272021a380
Csaba Hegedus <hegcsa1207@gmail.com>
2020-07-18 17:32:44 +0200

    trying out how to create a Task implementing Runnable interface and overrideing run method, how to create a Thread, how to start a Thread and how to make a Thread to sleep

---
+++ Concurrency/FirstThreads/src/threads/first/FirstThreads.java
@@ -0,0 +1,22 @@
+package threads.first;
+
+import java.util.concurrent.TimeUnit;
+
+public class FirstThreads {
+
+	public static void main(String[] args) {
+		Task task = new Task();
+		Thread thread = new Thread(task);
+		thread.start();
+		
+		try {
+			Thread.sleep(3000);
+			TimeUnit.SECONDS.sleep(3);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Inside main...");
+	}
+
+}
---
+++ Concurrency/FirstThreads/src/threads/first/Task.java
@@ -0,0 +1,20 @@
+package threads.first;
+
+public class Task implements Runnable {
+
+	@Override
+	public void run() {
+		System.out.println("Inside run...");
+		go();
+	}
+
+	private void go() {
+		System.out.println("Inside go...");
+		more();
+	}
+
+	private void more() {
+		System.out.println("Inside more...");
+	}
+
+}
